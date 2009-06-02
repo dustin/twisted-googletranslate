715b7a9c1ad9ab8baba665f47c867d095118ddee
Gabriele <carretta63@gmail.com>
2009-06-02 20:27:54 +0200

    first commit

---
+++ lib/translate.py
@@ -0,0 +1,37 @@
+
+import simplejson
+
+from transly.languages import Translation, Language
+from urllib import urlencode
+from twisted.web import client
+from twisted.internet import defer, reactor
+
+GOOGLE_TRANSLATE_API_URL = 'http://ajax.googleapis.com/ajax/services/language/translate?%s'
+
+class Translate(object):
+    
+    def __init__(self, language_origin, language_destiny):
+        self.lo = language_origin
+        self.ld = language_destiny
+        self.translation = Translation(self.lo, self.ld)
+        
+    def _create_url(self, word):
+        params = urlencode({"q":word, "langpair":str(self.translation), 
+                            "hl":"it","ie":"UTF-8", "v":"1.0", "oe":"UTF-8"})
+        request_url = GOOGLE_TRANSLATE_API_URL % params
+        return request_url
+    
+    def translate(self, word):
+        
+        def gotResponse(r):
+            json = simplejson.loads(r)
+            translated_text = json[u'responseData'][u'translatedText']
+            rv.callback(translated_text)
+                
+        rv = defer.Deferred()
+        response_url = self._create_url(word)
+        response = client.getPage(response_url,method='GET') \
+                         .addCallback(gotResponse) \
+                         .addErrback(lambda e: rv.errback(e))
+        return rv
+                                                            
